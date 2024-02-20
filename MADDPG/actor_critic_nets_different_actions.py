03fd0135ba0f157f40cbc3fd6e54f309c159bfb4
Cristian Morasso <morassocristian0@gmail.com>
2024-02-20 14:33:10 +0100

    modified file for multi actions

---
+++ MADDPG/actor_critic_nets_different_actions.py
@@ -0,0 +1,124 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+#import wandb
+
+class Actor(nn.Module):
+    def __init__(self, state_dim, action_dim, name, hidden_dim=256, chkpt_dir='tmp'):
+        super(Actor, self).__init__()
+
+        self.fc1 = nn.Linear(state_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
+        self.fc3 = nn.Linear(hidden_dim, action_dim)
+        self.name = name
+        self.chkpt_dir = chkpt_dir
+        self.optimizer = torch.optim.Adam(self.parameters(), lr=0.01)
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        self.to(self.device)
+        
+
+
+
+    def forward(self, state):
+        # x = x.view(1, -1)
+        x = F.relu(self.fc1(state))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        x = torch.softmax(x, dim=1)
+        return x
+    
+    def save_checkpoint(self):
+        #print('... saving checkpoint ...')
+        torch.save(self.state_dict(), self.chkpt_dir+'/'+self.name+'.pth')
+
+    def load_checkpoint(self):
+        print('... loading checkpoint ...')
+        self.load_state_dict(torch.load(self.chkpt_dir+'/'+self.name+ '.pth', map_location = torch.device("cpu")))
+
+class Critic(nn.Module):
+    def __init__(self, critic_dims, action_dim, n_agents, name, hidden_dim=256, chkpt_dir='tmp'):
+        super(Critic, self).__init__()
+
+        self.fc1 = nn.Linear(critic_dims, hidden_dim)#n_agents* action_dim
+        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
+        self.fc3 = nn.Linear(hidden_dim, 1)
+        self.chkpt_dir = chkpt_dir
+        self.name = name
+        self.optimizer = torch.optim.Adam(self.parameters(), lr=0.01)
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        self.to(self.device)
+
+
+    def forward(self, state, action):
+        x = torch.cat([state, action], dim=1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+    
+    def save_checkpoint(self):
+        #print('... saving checkpoint ...')
+        torch.save(self.state_dict(), self.chkpt_dir+'/'+self.name+'.pth')
+
+    def load_checkpoint(self):
+        print('... loading checkpoint ...')
+        self.load_state_dict(torch.load(self.chkpt_dir+'/'+self.name+'.pth', map_location = torch.device("cpu")))   
+
+class Agent:
+    def __init__(self, actor_dims, critic_dims, n_actions,n_agents, agent_idx, chkpt_dir='tmp', \
+                 gamma=0.95, tau=0.01, seed = 0, noise_func = None):
+        self.gamma = gamma
+        self.tau = tau
+        self.n_actions = n_actions
+        self.name = "agent_"+str(agent_idx)
+        torch.manual_seed(seed)
+        torch.backends.cudnn.deterministic = True
+
+        self.actor = Actor(actor_dims, n_actions, self.name+'_actor', chkpt_dir=chkpt_dir, hidden_dim=64)
+        self.critic = Critic(critic_dims, n_actions, n_agents=n_agents, name=self.name+'_critic', chkpt_dir=chkpt_dir,hidden_dim=128)
+
+        self.target_actor = Actor(actor_dims, n_actions, self.name+'_target_actor', chkpt_dir=chkpt_dir,hidden_dim=64)
+        self.target_critic = Critic(critic_dims, n_actions, n_agents=n_agents, name=self.name+'_target_critic', chkpt_dir=chkpt_dir,hidden_dim=128)
+        if noise_func is None:
+            self.noise_func = lambda x: 0.1
+        else:
+            self.noise_func = noise_func
+
+
+        self.update_target_networks(tau=self.tau)
+
+    def update_target_networks(self, tau=None):
+        if tau is None:
+            tau = self.tau
+        for target_param, param in zip(self.target_actor.parameters(), self.actor.parameters()):
+            target_param.data.copy_(tau * param.data + (1 - tau) * target_param.data)
+        for target_param, param in zip(self.target_critic.parameters(), self.critic.parameters()):
+            target_param.data.copy_(tau * param.data + (1 - tau) * target_param.data)
+
+
+    def get_action(self, state, eval=False, ep=1, max_ep=100, WANDB=False):
+        state = torch.tensor(state, dtype=torch.float32, device=self.actor.device)
+        action = self.actor(state).cpu().data.numpy()
+        noise = self.noise_func(ep, max_ep)
+        # if WANDB:
+        #     wandb.log({"noise": noise, 'ep_noise': ep})
+        # print(noise)
+        if not eval:
+            action += np.random.normal(0, noise, size=self.n_actions)
+        return action.clip(0,1)
+
+    def save_models(self):
+        self.actor.save_checkpoint()
+        self.critic.save_checkpoint()
+        self.target_actor.save_checkpoint()
+        self.target_critic.save_checkpoint()
+
+    def load_models(self):
+        self.actor.load_checkpoint()
+        self.critic.load_checkpoint()
+        self.target_actor.load_checkpoint()
+        self.target_critic.load_checkpoint()    
+
+
+
---
+++ MADDPG/main_different_actions.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python3
+import collections
+import numpy as np
+from pettingzoo.mpe import simple_adversary_v3, simple_push_v3, simple_v3, simple_spread_v3, simple_speaker_listener_v4
+from MADDPG import MADDPG
+from ma_replay_buffet import MultiAgenReplayBuffer
+from argParser import parse_args
+#import wandb
+import torch
+import gym
+import pandas as pd
+import os
+def dict_to_list(a):
+    groups = []
+    for item in a:
+        groups.append(list(item.values()))
+    return  groups
+args = parse_args()
+INFERENCE = False
+PRINT_INTERVAL = 5000
+MAX_EPISODES = args.n_ep
+BATCH_SIZE = args.batch_size
+MAX_STEPS = 25
+SEED = args.seed
+BUFFER_SIZE = args.buffer_size
+total_steps = 0
+score = -10
+best_score = -100
+score_history = []
+WANDB = False
+
+project_name = "MADDPG"
+params = f"_{args.mod_params}"
+env_name = "simple_speaker_listener_v4"
+env_class= simple_speaker_listener_v4
+# env = simple_adversary_v3.env()
+# if WANDB:
+#     wandb.init(
+#         project=project_name,
+#         name=f"{env_name}_fastUp_{SEED}",
+#         group=env_name, 
+#         job_type=env_name,
+#         reinit=True
+#     )
+if not os.path.isdir(f"nets/{env_name}{params}"):
+    os.mkdir(f"nets/{env_name}{params}")
+import sys
+sys.stdout = open('file_out.txt', 'w')
+# print('Hello World!')
+if INFERENCE:
+    env = env_class.parallel_env(max_cycles=100, continuous_actions=True, render_mode="human")
+else:
+    env = env_class.parallel_env(continuous_actions=True)
+    
+# env = gym.wrappers.RecordEpisodeStatistics(env)
+obs = env.reset(seed=SEED)
+print(env)
+print("num agents ", env.num_agents)
+# print("observation space ", env.observation_spaces)
+# print("action space ", env.action_spaces)
+# print(obs)
+np.random.seed(SEED)
+torch.manual_seed(SEED)
+torch.backends.cudnn.deterministic = True
+
+n_agents = env.num_agents
+actor_dims = []
+action_dim = []
+for i in range(n_agents):
+    actor_dims.append(env.observation_space(env.agents[i]).shape[0])#s[list(env.observation_spaces.keys())[i]].shape[0])  
+    action_dim.append(env.action_space(env.agents[i]).shape[0])#s[list(env.action_spaces.keys())[i]].shape[0])
+critic_dims = sum(actor_dims)
+# action_dim = env.action_space(env.agents[0]).shape[0]
+maddpg = MADDPG(actor_dims, critic_dims+sum(action_dim), n_agents, action_dim,chkpt_dir="nets", scenario=f"/{env_name}{params}", seed=SEED, args=args)
+if INFERENCE:
+    maddpg.load_checkpoint()
+memory = MultiAgenReplayBuffer(critic_dims, actor_dims, action_dim,n_agents, batch_size=BATCH_SIZE, buffer_size=BUFFER_SIZE,seed = SEED)
+# seed = 0
+rewards_history = []
+rewards_tot = collections.deque(maxlen=100)
+for i in range(MAX_EPISODES):
+    step = 0
+    obs, info = env.reset(seed=SEED+i)
+    obs=list(obs.values())
+    done = [False] * n_agents
+    rewards_ep_list = []
+
+    
+    # for agent in env.agent_iter():
+    # observation, reward, termination, truncation, info = env.last()
+    
+    score = 0
+    while  not any(done):#env.agents or
+        # obss = [[ob] for ob in obs.values()]
+        # obs = [ob for ob in obs.values()]
+        # print(step, total_steps, i)
+        
+        actions = maddpg.choose_action(obs, INFERENCE, ep=i, max_ep=MAX_EPISODES, WANDB=WANDB)
+        actions_dict = {agent:action.reshape(-1) for agent, action in zip(env.agents, actions)}
+        data = env.step(actions_dict)
+        data_processed = dict_to_list(data)
+        obs_, rewards, terminations, truncations, info = data_processed
+        done = (terminations or truncations)
+        # if INFERENCE and done:
+        #     env.render(render_mode="human")
+        if step >= MAX_STEPS-1 and not INFERENCE:
+            #print("MAX STEPS REACHED")
+            done = [True] * n_agents
+            # break
+        if not INFERENCE:
+            memory.store_transition(obs, actions, rewards, obs_, done)
+        
+        if (not INFERENCE) and total_steps % args.learn_delay == 0:
+            maddpg.learn(memory)
+        obs = obs_
+        rewards_ep_list.append(rewards) 
+        score += rewards[0]#+rewards["agent_1"]) #sum(rewards.values())
+        step += 1
+        total_steps += 1
+    score_history.append(score)
+    #rewards_history.append(np.sum(rewards_ep_list, axis=0))
+    rewards_tot.append(sum(rewards))
+    # print('episode ', i, 'score %.1f' % score, 'memory length ', len(memory))
+    avg_score = np.mean(rewards_tot)
+    rewards_history.append(avg_score)
+    # if WANDB and i % 100 == 0:    
+    #     wandb.log({#'avg_score_adversary':np.mean(np.array(rewards_history)[:,0][0]),\
+    #             # 'avg_score_agents':np.mean(np.array(rewards_history)[:,0][0]),\
+    #             # 'avg_score_agent1':np.mean(np.array(rewards_history)[:,1][0]),\
+    #             'total_rew':avg_score,'episode':i} )
+           
+    if i > 2000 and avg_score > best_score:
+        print("episode: ", i, "avg: ", avg_score, "best: ", best_score)
+        best_score = avg_score
+        if not INFERENCE:
+            print("Saving best model")
+            maddpg.save_checkpoint()
+    if i % PRINT_INTERVAL == 0 and i > 0:
+        print('episode ', i, 'score %.1f' % score, 'avg score %.1f' % avg_score)
+
+
+        # 
+
+reward_history_df = pd.DataFrame(rewards_history)
+reward_history_df.to_csv(f"{env_name}{params}.csv")
+print("-----END-----")
+sys.stdout.close()
